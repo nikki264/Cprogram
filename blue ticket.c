9ce7ec3a4cf051bbc59a0be90bb25480a79ccfc3
nikki264 <nikhila2815@gmail.com>
2026-02-13 21:31:21 +0530

    Add files via upload

---
+++ blue ticket.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main() 
+{
+int a,b ,c;
+printf("enter the value in A:");
+scanf("%d",&a);
+printf("enter the value in B:");
+scanf("%d",&b);
+printf("enter the value in C:");
+scanf("%d",&c);
+int ab=a+b,bc =b+c,ca=c+a;
+ if(ab==10||bc==10||ca==10){
+ printf("10");
+ }
+ else if(a>10||b>10||c>10){
+ printf("5");
+ }
+ else
+ {
+ printf("0");
+ }
+    return 0;
+}
