b3c37991ec4c10dd27f3c6ad2f746dde6c4a1c75
nikki264 <nikhila2815@gmail.com>
2026-02-17 22:35:12 +0530

    Add files via upload

---
+++ incrementop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int a= 65;
+int b=++a;
+int  c =((++a) -(b--));
+int d =((a++) +(--b) +(c++));
+    printf("%d",(a+b+c+d));
+    return 0;
+}
